eec6e1bfa1bb97072bd570510b2129baa0ae0621
Maxim Mineev <mms1401@ya.ru>
2024-02-28 18:37:15 +0500

    tsc init, add binary search

---
+++ binary_serach.ts
@@ -0,0 +1,29 @@
+// Бинарный поиск
+
+const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let count: number = 0;
+
+const binarySearch = (array: number[], item: number): number => {
+	let start: number = 0;
+	let end: number = array.length;
+	let middle: number;
+	let found: boolean = false;
+	let position: number = -1;
+
+	while(!found && start <= end) {
+		count += 1;
+		middle = Math.floor((start + end) / 2);
+		if (array[middle] === item) {
+			found = true;
+			position = middle;
+			return position;
+		}
+		if (item < array[middle]) {
+			end = middle - 1;
+		} else {
+			start = middle + 1;
+		}
+	}
+
+	return position;
+}
