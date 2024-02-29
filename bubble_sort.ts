dc5abb8c80cceedf4698a2a5871f24819141c8b4
Maxim Mineev <mms1401@ya.ru>
2024-02-29 23:16:49 +0500

    Update bubble_sort.ts

--- bubble_sort.ts
+++ bubble_sort.ts
@@ -5,7 +5,7 @@ let count = 0;
 
 const bubbleSort = (array: number[]): number[] => {
 	for (let i = 0; i < array.length; i++) {
-		for (let j = 0; j < array.length; j++) {
+		for (let j = 0; j < array.length - 1; j++) {
 			if (array[j + 1] < array[j]) {
 				[array[j], array[j + 1]] = [array[j + 1], array[j]];
 				count++;
